3dd004a0687c2effe189eb3a9c9cf3db78a533f8
Steven Del Rosario <stevendelro@pm.me>
2018-09-19 15:08:03 -0700

    Refactored App into a class    that extends React.Component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,27 @@ const Pet = props => {
   ]);
 };
 
-
-const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Kygo',
-      animal: 'dog',
-      breed: 'Labrador'
-    }),
-    React.createElement(Pet, {
-      name: 'Rio',
-      animal: 'dog',
-      breed: 'Australian Shepherd'
-    }),
-    React.createElement(Pet, {
-      name: 'George',
-      animal: 'tortoise',
-      breed: 'Pinta Island'
-    })
-  ]);
-};
+class App extends React.Component {
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement('h1', {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: 'Kygo',
+        animal: 'dog',
+        breed: 'Labrador'
+      }),
+      React.createElement(Pet, {
+        name: 'Rio',
+        animal: 'dog',
+        breed: 'Australian Shepherd'
+      }),
+      React.createElement(Pet, {
+        name: 'George',
+        animal: 'tortoise',
+        breed: 'Pinta Island'
+      })
+    ]);
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
