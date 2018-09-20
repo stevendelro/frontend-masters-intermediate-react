1a00bb2f9b1884676cfd5a454fa3572c1c8f7729
Steven Del Rosario <stevendelro@pm.me>
2018-09-20 14:34:01 -0700

    Removed handleTitleClick.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert('You clicked the title.');
-  }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <h1>Adopt Me!</h1>
         <Pet name="Kygo" animal="dog" breed="Labrador" />
         <Pet name="Rio" animal="dog" breed="Australian Shepherd" />
         <Pet name="George" animal="tortoise" breed="Pinta Island" />
