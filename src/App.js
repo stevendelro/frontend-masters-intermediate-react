abcea6f2f320bae9ca93c211287eb846bb2d1da3
Steven Del Rosario <stevendelro@pm.me>
2018-09-20 14:12:45 -0700

    Refactor vanilla React into JSX.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
 import Pet from './Pet';
 
 class App extends React.Component {
@@ -7,28 +7,14 @@ class App extends React.Component {
     alert('You clicked the title.');
   }
   render() {
-    return React.createElement('div', {}, [
-      React.createElement(
-        'h1',
-        { onClick: this.handleTitleClick },
-        'Adopt Me!'
-      ),
-      React.createElement(Pet, {
-        name: 'Kygo',
-        animal: 'dog',
-        breed: 'Labrador'
-      }),
-      React.createElement(Pet, {
-        name: 'Rio',
-        animal: 'dog',
-        breed: 'Australian Shepherd'
-      }),
-      React.createElement(Pet, {
-        name: 'George',
-        animal: 'tortoise',
-        breed: 'Pinta Island'
-      })
-    ]);
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Kygo" animal="dog" breed="Labrador" />
+        <Pet name="Rio" animal="dog" breed="Australian Shepherd" />
+        <Pet name="George" animal="tortoise" breed="Pinta Island" />
+      </div>
+    );
   }
 }
 
