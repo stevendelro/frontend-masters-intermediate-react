9903f0008d4f9bce633f88ccaf22de629db75251
Steven Del Rosario <stevendelro@pm.me>
2018-09-21 14:30:21 -0700

    Details.js will now display the selected pet's detailed info.

--- src/Details.js
+++ src/Details.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
+import pf from 'petfinder-client';
+import { navigate } from '@reach/router';
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: 'full',
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(', ');
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        navigate('/');
+      });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <h1>Loading..</h1>;
+    }
+    const { name, animal, breed, location, description } = this.state;
+
     return (
-      <div>
-        <h1>Details Page</h1>
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
       </div>
     );
-  };
-};
+  }
+}
 
 export default Details;
