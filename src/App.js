c749ceb4d0c83c15cc8b7f68fa207e1ddf15af44
Steven Del Rosario <stevendelro@pm.me>
2018-09-21 12:47:00 -0700

    Refactored App.js to utilize Reach Router.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { render} from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Link } from '@reach/router';
 import Results from './Results';
+import Details from './Details';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Adopt Me!</h1>
-        <Results />
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
-    )
+    );
   }
 }
 
