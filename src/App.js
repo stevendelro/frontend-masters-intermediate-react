247feb893960698abda52203bc787bd4ba23a2dc
Steven Del Rosario <stevendelro@pm.me>
2018-09-20 17:09:56 -0700

    Reworked Pet.js to handle the data coming down from App.js

--- src/App.js
+++ src/App.js
@@ -55,6 +55,8 @@ class App extends React.Component {
                 animal={pet.animal}
                 name={pet.name}
                 breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
               />
             );
           })}
--- src/Pet.js
+++ src/Pet.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Pet = props => {
-  // return React.createElement('div', {}, [
-  //   React.createElement('h1', {}, props.name),
-  //   React.createElement('h2', {}, props.animal),
-  //   React.createElement('h2', {}, props.breed)
-  // ]);
+class Pet extends Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
 
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
-
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === 'pn')
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    )
+  }
+}
 export default Pet;
