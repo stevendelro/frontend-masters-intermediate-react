5fca1ae7e89b3e3a3a10907f73af50299710d8c3
Steven Del Rosario <stevendelro@pm.me>
2018-09-20 17:01:45 -0700

    On DidMount, we reach out to the  PetFinder API to display some data.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import { render } from 'react-dom';
+import pf from 'petfinder-client';
 import Pet from './Pet';
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: 'full', location: 'Tillamook, OR' })
+      .then(data => {
+        let pets;
+        // The data is being translated from XML to JSON, thus this logic is needed
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            // If the data coming back is an array, great. Set it to: pets.
+            pets = data.petfinder.pets.pet;
+          } else {
+            // If the data coming back isn't in an array, let's put it in one.
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          // Otherwise, return "pets" set to an empty array.
+          pets = [];
+        }
+        this.setState({ pets });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Kygo" animal="dog" breed="Labrador" />
-        <Pet name="Rio" animal="dog" breed="Australian Shepherd" />
-        <Pet name="George" animal="tortoise" breed="Pinta Island" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(', ');
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
