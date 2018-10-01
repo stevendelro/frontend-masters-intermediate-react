5ad6d9bb358f35fc287990221d56d349e64a2a3f
Steven Del Rosario <stevendelro@pm.me>
2018-10-01 16:51:47 -0700

    Created SearchContext

---
+++ src/SearchContext.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchContext = React.createContext({
+  location: "Portland, OR",
+  animal: "",
+  breed: "",
+  breeds: [],
+  handleAnimalChange() {},
+  handleBreedChange() {},
+  handleLocationChange() {},
+  getBreeds() {}
+});
+
+export const Provider = SearchContext.Provider;
+export const Consumer = SearchContext.Consumer;
