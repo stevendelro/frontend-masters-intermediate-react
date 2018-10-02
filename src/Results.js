3b831a80ad7d0e6db04037b928f693e1dc36941f
Steven Del Rosario <stevendelro@pm.me>
2018-10-02 13:33:17 -0700

    Extrctd search logic into its own function,set DidMount to call search()

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import pf from 'petfinder-client';
+import { Consumer } from './SearchContext';
 import Pet from './Pet';
+import SearchBox from './SearchBox';
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -16,8 +18,17 @@ class Results extends Component {
     };
   }
   componentDidMount() {
+    this.search();
+  }
+
+  search = () => {
     petfinder.pet
-      .find({ output: 'full', location: 'Portland, OR' })
+      .find({
+        output: 'full',
+        location: this.props.searchParams.location,
+        animal: this.props.searchParams.animal,
+        breed: this.props.searchParams.breed
+      })
       .then(data => {
         let pets;
         if (data.petfinder.pets && data.petfinder.pets.pet) {
@@ -35,6 +46,7 @@ class Results extends Component {
   render() {
     return (
       <div className="search">
+        <SearchBox search={this.search} />
         {this.state.pets.map(pet => {
           let breed;
           if (Array.isArray(pet.breeds.breed)) {
@@ -59,4 +71,10 @@ class Results extends Component {
   }
 }
 
-export default Results;
+export default function ResultsWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  );
+}
