6d93328d7e090c71f7bb16907dc1ec135597f953
Steven Del Rosario <stevendelro@pm.me>
2018-10-02 13:32:01 -0700

    Reworked SearchParams to display SearchBox.

---
+++ src/SearchParams.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBox from './SearchBox';
+import { navigate } from '@reach/router';
+
+class SearchParams extends React.Component {
+  handleSearchSubmit() {
+    navigate('/');
+  }
+  render() {
+    return (
+      <div className="search-route">
+        <SearchBox search={this.handleSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default SearchParams;
