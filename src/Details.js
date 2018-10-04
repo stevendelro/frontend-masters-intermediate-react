f2da78080e94c939a25baf6b59d0dc5d3e591aad
Steven Del Rosario <stevendelro@pm.me>
2018-10-04 09:30:50 -0700

    Implemented a confirmation modal for the Adopt button with a Portal.

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import pf from 'petfinder-client';
 import { navigate } from '@reach/router';
 import Carousel from './Carousel';
+import Modal from './Modal';
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -10,8 +11,11 @@ const petfinder = pf({
 
 class Details extends Component {
   state = {
-    loading: true
+    loading: true,
+    showModal: true
   };
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
 
   componentDidMount() {
     petfinder.pet
@@ -46,7 +50,15 @@ class Details extends Component {
     if (this.state.loading) {
       return <h1>Loading..</h1>;
     }
-    const { name, animal, breed, location, description, media } = this.state;
+    const {
+      name,
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
@@ -56,7 +68,17 @@ class Details extends Component {
           <h2>
             {animal} - {breed} - {location}
           </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yup</button>
+                <button onClick={this.toggleModal}>Nope</button>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
