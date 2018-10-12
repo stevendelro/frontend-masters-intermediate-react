53c30162213371efa7578ebe6fc76d081ac95fd5
Steven Del Rosario <stevendelro@pm.me>
2018-10-12 11:32:37 -0700

    Incorporated breed and breeds into our index.js file.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import location from './location';
+import breeds from './breeds';
+import breed from './breed';
 import animal from './animal';
 
 export default combineReducers({
   location,
+  breeds,
+  breed,
   animal
 });
